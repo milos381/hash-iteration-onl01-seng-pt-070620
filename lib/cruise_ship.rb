3c59614e6f50684a143b4bb956d0e308f2a24ca9
Milos Popovic <milos_popovic@outlook.com>
2020-07-12 17:02:38 +0000

    Done.

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,11 +1,18 @@
 # passengers = {
-# suite_a: "Amanda Presley", 
-# suite_b: "Seymour Hoffman", 
-# suite_c: "Alfred Tennyson", 
-# suite_d: "Charlie Chaplin", 
+# suite_a: "Amanda Presley",
+# suite_b: "Seymour Hoffman",
+# suite_c: "Alfred Tennyson",
+# suite_d: "Charlie Chaplin",
 # suite_e: "Crumpet the Elf"
 # }
 
 def select_winner(passengers)
-  # add the code snippet here!
+  winner = ""
+  passengers.each do |suite, name|  #each => just the value
+                                    #collect would be key and value
+    if suite == :suite_a && name.start_with?("A")
+      winner = name
+    end
+  end
+  winner #we now return the name of a winner
 end
